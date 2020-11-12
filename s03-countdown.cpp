57bbc2db068a9e6b8e299ac04a22857b9ccfcb20
Marceli Gosztyła <s23319@pjwstk.edu.pl>
2020-11-12 16:52:28 +0100

    Add exercises

---
+++ s03-countdown.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+ auto main(int, char* argv[]) -> int
+{
+  
+   auto liczba= std::stoi(argv[1]);
+   //std::cout << "jesteśmy tu\n*;
+   for (int i = liczba; i >= 0; --i) {
+       std::cout << i <<"... \n";
+   }
+   return 0;
+}
